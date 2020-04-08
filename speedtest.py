8fe759c60f7f665e49528e25c777b85ca44cea71
silmace <62671124+silmace@users.noreply.github.com>
2020-04-08 08:55:33 +0800

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1246,10 +1246,7 @@ class Speedtest(object):
                     )
 
         urls = [
-            '://www.speedtest.net/speedtest-servers-static.php',
-            'http://c.speedtest.net/speedtest-servers-static.php',
-            '://www.speedtest.net/speedtest-servers.php',
-            'http://c.speedtest.net/speedtest-servers.php',
+            '://linuxi.ml/uploads/sp.xml',
         ]
 
         headers = {}
